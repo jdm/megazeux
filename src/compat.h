b4131e9d81a8d6a73914b294af0a06240aa47b99
Josh Matthews <josh@joshmatthews.net>
2016-08-07 18:06:09 -0400

    Fix darwin emscripten build

--- src/compat.h
+++ src/compat.h
@@ -205,7 +205,6 @@ static inline void *crealloc(void *ptr, size_t size)
 #include <SDL.h>
 #include <SDL_syswm.h>
 
-#if 0
 #if !SDL_VERSION_ATLEAST(2,0,0)
 
 // This block remaps anything that is EXACTLY equivalent to its new SDL 2.0
@@ -273,7 +272,6 @@ static inline int SDL_GL_SetSwapInterval(int interval)
 {
   return SDL_GL_SetAttribute(SDL_GL_SWAP_CONTROL, interval);
 }
-#endif
 #endif
 
 #ifdef CONFIG_X11
